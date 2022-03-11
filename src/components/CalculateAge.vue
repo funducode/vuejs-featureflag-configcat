f75c64747668fe283e6ddcecb87e6928aec3c921
Anand Supner <funducode@hotmail.com>
2022-03-11 06:40:54 -0500

    test flag that hides textbox

--- src/components/CalculateAge.vue
+++ src/components/CalculateAge.vue
@@ -13,10 +13,112 @@
       </p>
       <input type="number" v-model="birthYear" />
       <button class="btn btn-calculate" @click="calcAge">Calculate</button>
-      <button class="btn" @click="getUserAgeFeatureStatus">
-        Calculate Age
-      </button>
       <p v-if="age">You are {{ age }} years old</p>
     </div>
   </div>
 </template>
+
+<script>
+import * as configCat from "configcat-js";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      userAgeFeature: null,
+      error: null,
+      birthYear: "",
+      age: "",
+      loading: true,
+    };
+  },
+  methods: {
+    // Get feature status from ConfigCat
+    async getUserAgeFeatureStatus() {
+      try {
+        // Seeing all log messages makes the first integration easier. When the integration is done you can remove this line to avoid too detailed logging in your application.
+        let logger = configCat.createConsoleLogger(3); // Setting log level to 3 (Info)
+
+        let configCatClient = configCat.createClient(
+          "BwPaCO2USkqGz554uC6qeA/578pqJ9x7UiB973ct1ttCw",
+          {
+            // <-- This is the actual SDK Key for your Production environment
+            logger: logger,
+          }
+        );
+
+        const calculateuseragefeature = await configCatClient.getValueAsync(
+          "calculateuseragefeature",
+          false
+        );
+        console.log("calculateuseragefeature: " + calculateuseragefeature);
+        this.userAgeFeature = calculateuseragefeature;
+      } catch (err) {
+        this.error = err.message;
+      }
+      this.loading = false;
+    },
+    // Calculate age Feature
+    calcAge() {
+      const age = 2021 - this.birthYear;
+      this.age = age;
+      this.birthYear = "";
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  text-align: center;
+
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn {
+  color: rgb(16, 82, 54);
+  background-color: royalblue;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 15px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  font-weight: 600;
+}
+
+.btn:hover {
+  background-color: rgb(16, 82, 54);
+  color: #fff;
+}
+
+.btn:focus-visible,
+input:focus-visible {
+  outline: none;
+}
+
+.btn-calculate {
+  padding: 10px 25px;
+  margin-left: 8px;
+  border-radius: 8px;
+}
+
+input {
+  padding: 10px 25px;
+  font-size: 1.2rem;
+  border-radius: 8px;
+  border: none;
+}
+
+.text {
+  font-size: 1.5em;
+  color: #fff;
+}
+</style>
